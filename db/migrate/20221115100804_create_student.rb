460dbfe1230a01429e2c7747c9791ecd081dcfaf
Enotik98 <poliakitti@gmail.com>
2022-11-18 22:22:19 +0200

    gem

--- db/migrate/20221115100804_create_student.rb
+++ db/migrate/20221115100804_create_student.rb
@@ -1,6 +1,6 @@
 class CreateStudent < ActiveRecord::Migration[7.0]
   def change
-    create_table :student do |t|
+    create_table :students do |t|
       t.string :Surname
       t.string :Street
       t.string :House
